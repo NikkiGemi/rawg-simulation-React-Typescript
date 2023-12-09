ff41aec9e5ab14f4f1a71d4d6dd5df7a7438517d
NikkiGemi <niharika_gem@icloud.com>
2023-12-08 21:14:12 -0600

    show spinner while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,24 +1,29 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImage from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, error, loading } = useGenres();
+  if (error) return null;
+
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              borderRadius="2px"
-              boxSize="32px"
-              src={getCroppedImage(genre.image_background)}
-            ></Image>
-            <Text>{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {loading && <Spinner></Spinner>}
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                borderRadius="2px"
+                boxSize="32px"
+                src={getCroppedImage(genre.image_background)}
+              ></Image>
+              <Text>{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
