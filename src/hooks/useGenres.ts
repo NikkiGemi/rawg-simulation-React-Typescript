3419e38685ded759f80fb63c57aae0c299181ff0
NikkiGemi <niharika_gem@icloud.com>
2023-12-08 19:23:04 -0600

    Fetching and dispalying genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+export interface Genre {
+  id: number;
+  name: String;
+}
+
+interface FetchGenreResponse {
+  count: number;
+  results: Genre[];
+}
+
+const useGame = () => {
+  const [genres, setGames] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    apiClient
+      .get<FetchGenreResponse>("/genres", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, loading };
+};
+
+export default useGame;
